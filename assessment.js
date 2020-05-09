11c1025d3cc66dda81b2b5b4737edb9d6e99b44a
nbecr <65059265+nbecr@users.noreply.github.com>
2020-05-09 14:41:24 +0900

    #1 の修正を行いました。

--- assessment.js
+++ assessment.js
@@ -5,7 +5,7 @@ const resultDivided = document.getElementById('result-area');
 const tweetDivided = document.getElementById('tweet-area');
 
 /**
- * 指定した要素の子どもを全て除去する
+ * 指定した要素の子どもを全て除去する。
  * @param {HTMLElement} element HTMLの要素
  */
 function removeAllChildren(element) {
